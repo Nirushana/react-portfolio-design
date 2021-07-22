96948979a6078ad01ded762ae4aaff6459eacb4f
Nirushana <kavinda.n.t@gmail.com>
2021-07-22 19:43:56 +0530

    AboutSection

--- src/components/About/AboutSection.js
+++ src/components/About/AboutSection.js
@@ -1,7 +1,64 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const AboutContainer = styled.div`
- background-color: #333333;
- width: 100%;
- height: 100vh;
+  background-color: #333333;
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+`;
+
+export const AboutHeader = styled.div`
+position: relative;
+ margin-top: 100px;
+ margin-left: 100px;
 `
+
+export const AboutHeaderH = styled.h1`
+
+ color: #fff;
+ font-size: 2rem;
+ font-weight: bold;
+`
+
+export const AboutContent = styled.div`
+  width: 90%;
+  height: 100%;
+  margin: 0 auto;
+  max-width: 1000px;
+  display: flex;
+  margin-top: 10vh;
+  justify-content: center;
+  
+`;
+
+export const AboutLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  align-items: flex-start;
+  
+`;
+
+export const AboutP = styled.p`
+ color: #fff;
+ font-size: 1.5rem;
+ font-weight: 300;
+`
+
+export const AboutRight = styled.div`
+ flex: 8;
+`
+
+export const AboutImg = styled.img`
+  width: 100%;
+  height: 100%;
+  max-width: 260px;
+  height: 430px;
+  box-shadow: 9px 8px #fff;
+  margin-left: 30%;
+
+  @media only screen and (max-width: 768px) {
+    width: 220px;
+    height: 320px;
+  }
+`;
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,31 @@
 import React from "react";
-import { AboutContainer } from './AboutSection';
+import { AboutContainer, AboutHeader, AboutContent, AboutLeft, AboutP, AboutRight, AboutImg, AboutHeaderH } from "./AboutSection";
+import SideImg from '../../images/sub_image.jpeg';
 
 function About() {
   return (
-      <AboutContainer>
-
-      </AboutContainer>
-  )
+    <AboutContainer>
+      <AboutHeader>
+      <AboutHeaderH>About Me</AboutHeaderH>
+      </AboutHeader>
+      
+      <AboutContent>
+        <AboutLeft>
+          <AboutP>
+            Hi, I am Kavinda Nirushana. I am a Full Stack Software engineer
+            proficient in designing and development of both Web-based and
+            Mobile-based applications. As a full stack developer, I can help you
+            with everything you need to get your project off the ground, from
+            concept to launch.
+          </AboutP>
+          {/* <AboutSpan></AboutSpan> */}
+        </AboutLeft>
+        <AboutRight>
+          <AboutImg src={SideImg} />
+        </AboutRight>
+      </AboutContent>
+    </AboutContainer>
+  );
 }
 
 export default About;
